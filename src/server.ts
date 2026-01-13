56b45753229384c2bca00ea3ef41b72640c44af5
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-13 11:31:06 -0100

    chore(db):setup database with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import app from './app'
+import connectDB from './dbconfig/db'
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
+
+// Connect to the database before starting the server
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
