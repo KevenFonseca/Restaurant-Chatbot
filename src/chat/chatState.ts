877eda725892d19233b0efba25ee02c24a713678
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-19 21:24:06 -0100

    feat(chat): define chat states

---
+++ src/chat/chatState.ts
@@ -0,0 +1,9 @@
+export enum ChatState {
+    START = 'START',
+    MENU = 'MENU',
+    ORDERING = 'ORDERING',
+    CHECKOUT = 'CHECKOUT',
+    PAYMENT = 'PAYMENT',
+    COMPLETED = 'COMPLETED',
+    CANCELLED = 'CANCELLED'
+}
