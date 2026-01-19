91eb6812ec434c1323bca0f2140e99fbdd436ae1
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-19 21:16:19 -0100

    chore(server): Add router chatRouter

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { Request, Response } from 'express'
+import cookieParser from 'cookie-parser'
+import chatRouter from './chat/chatRouter'
 
 const app = express()
 
 // Middleware
 app.use(express.json())
+app.use(cookieParser())
+
+// Chat routes
+app.use('/chat', chatRouter)
 
 // Basic route
 app.get('/', (req: Request, res: Response) => {
