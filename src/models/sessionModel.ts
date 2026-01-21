c1c50629a6b36415f26ce60ccd33b0bda2afe6c5
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-21 19:23:16 -0100

    fix(models): add temporarOrder to the struture of the session schema

--- src/models/sessionModel.ts
+++ src/models/sessionModel.ts
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema
 const SessionSchema = new Schema({
   deviceId: { type: String, required: true },
   currentState: { type: String, required: true },
+  temporaryOrder: { type: Array, default: [] }
 },{
     timestamps: true
 })
