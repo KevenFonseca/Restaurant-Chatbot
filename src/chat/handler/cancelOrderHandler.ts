3b3f036958952e6c07b31122984270d49cd20cdf
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-25 14:12:40 -0100

    feat(chat): add cancel order handler

---
+++ src/chat/handler/cancelOrderHandler.ts
@@ -0,0 +1,32 @@
+import { ChatState } from '../chatState'
+import { ChatResponse } from '../../types/chatTypes'
+import Order from '../../models/orderModel'
+
+const cancelOrderHandler = async ({session }: {session: any}): Promise<ChatResponse> => {
+    if (!session.temporaryOrder || session.temporaryOrder.length === 0) {
+        return {
+            reply: 'You have no current order to cancel',
+            updatedSession: session
+        }
+    }
+
+    const order = await Order.findOne({
+        sessionId: session._id,
+        status: 'pending',
+    }).sort({ createdAt: -1})
+
+    if (order) {
+        order.status = 'canceled'
+        await order.save()
+    }
+
+    session.temporaryOrder = []
+    session.currentState = ChatState.MENU
+
+    return {
+        reply: 'Your current order has been canceled',
+        updatedSession: session
+    }
+}
+
+export default cancelOrderHandler
