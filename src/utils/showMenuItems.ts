5694580ea85588ecc3850e6ed470d6db2f0308f6
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-27 14:14:04 -0100

    feat(chat): add showMenuItems and move chat to ordering state

---
+++ src/utils/showMenuItems.ts
@@ -0,0 +1,22 @@
+import menuItemModel from "../models/menuItemModel"
+import { ChatResponse } from "../types/chatTypes"
+import { ChatState } from '../chat/chatState'
+
+const showMenuItems = async ({ session }: {session: any}): Promise<ChatResponse> => {
+    const menuItems = await menuItemModel.find({ isAvailable: true })
+
+    const menuList = menuItems
+        .map(item => `${item.id}. ${item.name} - $${item.price}`)
+        .join('\n')
+
+    const reply = `${menuList}\n\nPlace your order`
+
+    session.currentState = ChatState.ORDERING
+
+    return {
+        reply,
+        updatedSession: session
+    }
+}
+
+export { showMenuItems }
