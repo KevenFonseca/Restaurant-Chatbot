2969fa7bf93fc4c97959e43091881dc34843397c
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-28 08:34:15 -0100

    chore(dist): remove dist from repository

--- dist/models/menuItemModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-const MenuItemSchema = new Schema({
-    id: { type: Number, required: true, unique: true },
-    name: { type: String, required: true, unique: true },
-    price: { type: Number, required: true },
-    isAvailable: { type: Boolean, default: true },
-});
-export default mongoose.model('MenuItem', MenuItemSchema);
